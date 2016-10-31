83a0adfff67eb95baf4c12cfa1d410defda6d8b0
sampanna pokhrel <sampanna99@gmail.com>
2016-10-31 00:36:05 -0500

    Implement saving a gig

--- MusicLovers/Controllers/GigsController.cs
+++ MusicLovers/Controllers/GigsController.cs
@@ -1,10 +1,13 @@
-﻿using MusicLovers.Models;
+﻿using Microsoft.AspNet.Identity;
+using MusicLovers.Models;
 using MusicLovers.ViewModels;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
 namespace MusicLovers.Controllers
 {
+
     public class GigsController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -12,10 +15,31 @@ namespace MusicLovers.Controllers
         {
             _context = new ApplicationDbContext();
         }
+
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel { Genres = _context.Genres.ToList() };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            //var artist = _context.Users.Single(u => u.Id == artistId);
+            //var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
