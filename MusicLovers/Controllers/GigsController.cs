36bc573151273f74ff4a4b4e52f76a20fa75b3df
sampanna pokhrel <sampanna99@gmail.com>
2016-10-30 23:52:47 -0500

    add a viewmodel and a form

---
+++ MusicLovers/Controllers/GigsController.cs
@@ -0,0 +1,21 @@
+﻿using MusicLovers.Models;
+using MusicLovers.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MusicLovers.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel { Genres = _context.Genres.ToList() };
+            return View(viewModel);
+        }
+    }
+}
---
+++ MusicLovers/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MusicLovers.Models;
+using System.Collections.Generic;
+
+namespace MusicLovers.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
